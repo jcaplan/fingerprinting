1dbd197dbb56bbc5f5a1356273546a95d5ed5221
Jonah Caplan <jonah.caplan@mail.mcgill.ca>
2015-06-09 11:44:28 -0400

    changed address for cpu0 sw_reset

--- automotive_control/rtos/ucosMon_bsp/system.h
+++ automotive_control/rtos/ucosMon_bsp/system.h
@@ -4,7 +4,8 @@
  * Machine generated for CPU 'processorM_0_cpuM' in SOPC Builder design 'nios_fprint'
  * SOPC Builder design path: ../../nios_fprint.sopcinfo
  *
- * Generated: Tue Jun 09 11:37:47 EDT 2015
+ * Generated: Tue Jun 09 11:42:53 EDT 2015
+ */
 
 /*
  * DO NOT MODIFY THIS FILE
