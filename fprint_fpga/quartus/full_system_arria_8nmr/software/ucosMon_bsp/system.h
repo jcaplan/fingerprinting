8b5942169f79251008d6de712794b4629f9987f2
Harsh Aurora <harsh.aurora@emynmuil.ece.mcgill.ca>
2015-07-23 21:34:44 -0400

    bug fix in NMR software and milestone drivers

--- fprint_fpga/quartus/full_system_arria_8nmr/software/ucosMon_bsp/system.h
+++ fprint_fpga/quartus/full_system_arria_8nmr/software/ucosMon_bsp/system.h
@@ -4,7 +4,7 @@
  * Machine generated for CPU 'processorM_0_cpuM' in SOPC Builder design 'nios_fprint'
  * SOPC Builder design path: ../../nios_fprint.sopcinfo
  *
- * Generated: Mon Jul 20 09:02:51 EDT 2015
+ * Generated: Thu Jul 23 20:41:34 EDT 2015
  */
 
 /*
